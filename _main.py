ef5461749b2aa424b906eed9746d3b13576052f9
Ehsan Bikdeli <ebikdeli@gmail.com>
2024-11-12 19:53:04 +0330

    pyqt added

--- _main.py
+++ _main.py
@@ -1,10 +1,9 @@
+from tkinter import Tk, ttk
 
 
-
-def main():
-    """Main function to execute program"""
-    pass
-
-
-if __name__ == '__main__':
-    main()
+root: Tk = Tk(screenName='Network GUI', baseName='Network GUI management')
+frm = ttk.Frame(root, padding=200, height=500)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
